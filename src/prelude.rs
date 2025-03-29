cd93308ed12be2e0f10c620d4f18dc621a189fd4
Jagoda Estera Ślązak <jslazak@jslazak.com>
2025-03-29 19:08:36 +0100

    docs(api): add missing api docs

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,11 @@
+//! A prelude for conveniently writing applications using this library.
+//!
+//! # Examples
+//!
+//! ```rust,no_run
+//! use mousefood::prelude::*;
+//! ```
+
 pub use crate::EmbeddedBackend;
 pub use embedded_graphics::pixelcolor::{
     Bgr555, Bgr565, Bgr666, Bgr888, Rgb555, Rgb565, Rgb666, Rgb888,
