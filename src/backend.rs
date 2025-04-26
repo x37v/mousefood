f88d73b3e138db74f38e695e9aacc126671bce0e
Jagoda Estera Ślązak <jslazak@jslazak.com>
2025-04-26 19:09:18 +0200

    feat(modifiers): italic modifier support
    BREAKING CHANGE: `EmbeddedBackendConfig` now requires providing `font_italic`

--- src/backend.rs
+++ src/backend.rs
@@ -29,6 +29,8 @@ where
     pub font_regular: MonoFont<'static>,
     /// Bold font.
     pub font_bold: Option<MonoFont<'static>>,
+    /// Italic font.
+    pub font_italic: Option<MonoFont<'static>>,
 }
 
 impl<D, C> Default for EmbeddedBackendConfig<D, C>
@@ -41,6 +43,7 @@ where
             flush_callback: Box::new(|_| {}),
             font_regular: default_font::regular,
             font_bold: None,
+            font_italic: None,
         }
     }
 }
@@ -75,6 +78,7 @@ where
 
     font_regular: MonoFont<'static>,
     font_bold: Option<MonoFont<'static>>,
+    font_italic: Option<MonoFont<'static>>,
 
     char_offset: geometry::Point,
 
@@ -97,6 +101,7 @@ where
         #[cfg(feature = "simulator")] flush_callback: impl FnMut(&mut SimulatorDisplay<C>) + 'static,
         font_regular: MonoFont<'static>,
         font_bold: Option<MonoFont<'static>>,
+        font_italic: Option<MonoFont<'static>>,
     ) -> EmbeddedBackend<'display, D, C> {
         let pixels = layout::Size {
             width: display.bounding_box().size.width as u16,
@@ -109,6 +114,7 @@ where
             flush_callback: Box::new(flush_callback),
             font_regular,
             font_bold,
+            font_italic,
             char_offset: geometry::Point::new(0, font_regular.character_size.height as i32),
             columns_rows: layout::Size {
                 height: pixels.height / font_regular.character_size.height as u16,
@@ -139,6 +145,7 @@ where
             config.flush_callback,
             config.font_regular,
             config.font_bold,
+            config.font_italic,
         )
     }
 
@@ -186,7 +193,10 @@ where
                         Some(font) => style_builder.font(font),
                     },
                     style::Modifier::DIM => style_builder, // TODO
-                    style::Modifier::ITALIC => style_builder, // TODO
+                    style::Modifier::ITALIC => match &self.font_italic {
+                        None => style_builder.font(&self.font_regular),
+                        Some(font) => style_builder.font(font),
+                    },
                     style::Modifier::UNDERLINED => style_builder.underline(),
                     style::Modifier::SLOW_BLINK => style_builder, // TODO
                     style::Modifier::RAPID_BLINK => style_builder, // TODO
